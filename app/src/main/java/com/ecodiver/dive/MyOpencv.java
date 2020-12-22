cbf46e94330ee0a77348c9c0d0b5bda4d4ac3e91
suja477 <suja477@gmail.com>
2020-12-22 20:00:45 +0530

    Update MyOpencv.java

--- app/src/main/java/com/ecodiver/dive/MyOpencv.java
+++ app/src/main/java/com/ecodiver/dive/MyOpencv.java
@@ -7,7 +7,6 @@ import android.graphics.Bitmap;
 import org.opencv.core.MatOfFloat;
 import org.opencv.core.MatOfInt;
 import org.opencv.core.Scalar;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.*;
 
 import java.util.ArrayList;
@@ -67,7 +66,7 @@ public class MyOpencv {
         List<Mat> bgrPlanes = new ArrayList<>();
         Core.split(img, bgrPlanes);
         int histSize = 256;
-        float[] range = {0, 256}; //the upper boundary is exclusive
+        float[] range = {0, 256}; //the upper boundary is exclusive///----------check 256
         MatOfFloat histRange = new MatOfFloat(range);
         boolean accumulate = false;
         Mat bHist = new Mat(), gHist = new Mat(), rHist = new Mat();
@@ -77,7 +76,7 @@ public class MyOpencv {
 
       //  Core.normalize(bHist, bHist, 0, 256, Core.NORM_MINMAX);//check 256
       //  Core.normalize(gHist, gHist, 0, 256, Core.NORM_MINMAX);
-        Core.normalize(rHist, rHist, 0, 256, Core.NORM_MINMAX);
+       Core.normalize(rHist, rHist, 0, 255, Core.NORM_MINMAX);///----------check 256
 
      //   float[] bHistData = new float[(int) (bHist.total() * bHist.channels())];
      //   bHist.get(0, 0, bHistData);
@@ -85,8 +84,42 @@ public class MyOpencv {
      //   gHist.get(0, 0, gHistData);
         float[] rHistData = new float[(int) (rHist.total() * rHist.channels())];
         rHist.get(0, 0, rHistData);
+
         return rHistData;
     }
 
-    
+    public static ArrayList createHistogramBlueGreen(Mat img){
+        // Mat src = Imgcodecs.imread(M);
+        if (img.empty()) {
+            System.err.println("Cannot read image: " + img);
+            System.exit(0);
+        }
+        List<Mat> bgrPlanes = new ArrayList<>();
+        Core.split(img, bgrPlanes);
+        int histSize = 256;
+        float[] range = {0, 256}; //the upper boundary is exclusive
+        MatOfFloat histRange = new MatOfFloat(range);
+        boolean accumulate = false;
+        Mat bHist = new Mat(), gHist = new Mat(), rHist = new Mat();
+          Imgproc.calcHist(bgrPlanes, new MatOfInt(0), new Mat(), bHist, new MatOfInt(histSize), histRange, accumulate);
+         Imgproc.calcHist(bgrPlanes, new MatOfInt(1), new Mat(), gHist, new MatOfInt(histSize), histRange, accumulate);
+       // Imgproc.calcHist(bgrPlanes, new MatOfInt(2), new Mat(), rHist, new MatOfInt(histSize), histRange, accumulate);
+
+         Core.normalize(bHist, bHist, 0, 256, Core.NORM_MINMAX);//----------check 256
+         Core.normalize(gHist, gHist, 0, 256, Core.NORM_MINMAX);///----------check 256
+        //Core.normalize(rHist, rHist, 0, 256, Core.NORM_MINMAX);
+
+          float[] bHistData = new float[(int) (bHist.total() * bHist.channels())];
+          bHist.get(0, 0, bHistData);
+         float[] gHistData = new float[(int) (gHist.total() * gHist.channels())];
+          gHist.get(0, 0, gHistData);
+        //float[] rHistData = new float[(int) (rHist.total() * rHist.channels())];
+       // rHist.get(0, 0, rHistData);
+        ArrayList retturnArray=new ArrayList();
+        retturnArray.add(bHistData);
+        retturnArray.add(gHistData);
+        return retturnArray;
+    }
+
+
 }

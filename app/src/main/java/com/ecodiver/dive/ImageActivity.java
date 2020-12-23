44a81e4cfde73e9b5cff97a355d7741b67ea4281
suja477 <suja477@gmail.com>
2020-12-23 20:46:32 +0530

    Update ImageActivity.java

--- app/src/main/java/com/ecodiver/dive/ImageActivity.java
+++ app/src/main/java/com/ecodiver/dive/ImageActivity.java
@@ -1,12 +1,19 @@
 package com.ecodiver.dive;
 
 import androidx.appcompat.app.AppCompatActivity;
+import jp.co.cyberagent.android.gpuimage.GPUImage;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageGrayscaleFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageSepiaToneFilter;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +22,17 @@ public class ImageActivity extends AppCompatActivity {
 
     private Uri fileimagepath;
     private ImageView ivUpload;
+    private Button sepiaBtn;
+    private Button grayscaleBtn;
+    private  GPUImage gpuImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
-        ivUpload=findViewById(R.id.imageView);
+
+        grayscaleBtn=findViewById(R.id.btn_grayscale);
+        sepiaBtn=findViewById(R.id.btn_sepia);
         Intent imageIntent=getIntent();
         if(imageIntent.hasExtra("BUNDLE_EXTRA"))
         {
@@ -29,7 +41,48 @@ public class ImageActivity extends AppCompatActivity {
 
         }
 
-        Picasso.with(this).load(fileimagepath).fit().
-                centerCrop().into(ivUpload);
+        /*Picasso.with(this).load(fileimagepath).fit().
+                centerCrop().into(ivUpload);*/
+
+        GPUImageGrayscaleFilter gpuImageGrayscaleFilter=new GPUImageGrayscaleFilter();
+        Uri imageUri = fileimagepath;
+         gpuImage = new GPUImage(getApplicationContext());
+        gpuImage.setGLSurfaceView((GLSurfaceView) findViewById(R.id.gpusurfaceview));
+        gpuImage.setImage(imageUri); // this loads image on the current thread, should be run in a thread
+        grayscaleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                gpuImage.setFilter(gpuImageGrayscaleFilter);
+
+                // Later when image should be saved saved:
+               // gpuImage.saveToPictures("GPUImage", "ImageWithFilter.jpg", null);
+                }
+
+        });
+        GPUImageSepiaToneFilter gpuImageSepiaToneFilter=new GPUImageSepiaToneFilter();
+        sepiaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                gpuImage.setFilter(gpuImageSepiaToneFilter);
+
+                // Later when image should be saved saved:
+                // gpuImage.saveToPictures("GPUImage", "ImageWithFilter.jpg", null);
+            }
+
+        });
+    }
+    public void saveImage(View v){
+
+        gpuImage.saveToPictures("GPUImage", "ImageWithFilter.jpg", null);
+    }
+    public void shareImage(View v){
+
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, fileimagepath);
+
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,"EcoDiver");
+        startActivity(Intent.createChooser(sharingIntent,"choose one"));
     }
 }
